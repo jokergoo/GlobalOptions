36e69b9e55396feb1cccbd031ba5a5d4b06af313
jokergoo <jokergoo@gmail.com>
2014-10-07 20:36:12 +0200

    add tests

--- check.bat
+++
@@ -1,8 +0,0 @@
-perl -I../circlize/lib -MR::Comment2Man -e "R::Comment2Man->draft('R')"
-
-cd ..
-
-R CMD REMOVE GlobalOptions
-R CMD build --compact-vignettes=gs+qpdf GlobalOptions
-R CMD check --as-cran --timings GlobalOptions_0.0.2.tar.gz
-R CMD INSTALL GlobalOptions_0.0.2.tar.gz
